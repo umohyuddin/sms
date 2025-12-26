8a72c7d056fcaab83697f91ccf7a2a61fbc152e1
uzair <uzair.anwar@expertflow.com>
2025-12-26 17:02:58 +0500

    allow all origin

--- src/main/java/com/smartsolutions/eschool/util/jwt/WebConfig.java
+++ src/main/java/com/smartsolutions/eschool/util/jwt/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://192.168.100.50:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
